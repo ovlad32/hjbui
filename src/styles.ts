8aaca519151539ec6507a0130c3b2e789149e15c
ovlad32 <ovlad32@gmail.com>
2021-10-07 19:01:57 +0300

    initial

---
+++ src/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+
+export const AppContainer = styled.div`
+    align-items:flex-start;
+    display:flex;
+    height:100%;
+    padding:20px;
+    width:100%;
+
+    background-color:wheat;
+`
