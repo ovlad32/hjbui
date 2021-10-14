143992c8d2fc6d809b58236d519e39724ed162e7
ovlad32 <vlad@HP-Envy.localdomain>
2021-10-15 01:56:24 +0300

    work

--- be/src/index.js
+++ be/src/index.js
@@ -17,40 +17,50 @@ app.get("/grps", (req, res) => {
   });
 });
 
-app.get("/grps/:id", (req, res) => {
+app.get("/grps/:id/:mode", (req, res) => {
   try {
     console.log(req.params);
 
-    var grp = {};
-
     sqlite.all(
-      `select * from grps where id = $id`,
+      `select 
+        g.id as id,
+        g.name as name,
+        g.lake_agg_row_count,
+        g.base_agg_row_count,
+        g.agg_jnt_cardinality,
+        g.jc_1, g.jc_2, g.jc_3, g.jc_4, g.jc_5,
+        g.jc_6, g.jc_7, g.jc_8, g.jc_9, g.jc_10,
+        j.id as grp_jnt_id,
+        j.agg_cardinality,
+        jc.side,
+        c.column_name,
+        c.mtd_tab_id
+        from grps g
+         inner join grp_jnts j on j.grp_id = g.id
+         inner join grp_jnt_cols jc on jc.grp_jnt_id = j.id
+         inner join mtd_cols on c.id = jc.mtd_col_id
+      where id = $id`,
       {
         $id: req.params.id,
       },
       (err, rows) => {
         if (err) throw err;
-        rows.forEach((row) => {
-          grp = { ...row };
-          grp.jnts = [];
-          sqlite.all(
-            "select * from grp_jnts where grp_id = $grpId",
-            {
-              $grpId: row.id,
-            },
-            (err, rows) => {
-              if (err) throw err;
-              rows.forEach((row) => {
-                console.log(row);
-                grp.jnts.push({ ...row });
+
+        switch (res.params.mode) {
+          case "detailed":
+            res.json(rows);
+            break;
+          default:
+            rows.forEach((row) => {
+              res.json({
+                id: row.id,
+                name: row.name,
               });
-            }
-          );
-        });
+              return;
+            });
+        }
       }
     );
-    console.log(grp);
-    res.json(grp);
   } catch (err) {
     res.status(500).send(err);
   }
